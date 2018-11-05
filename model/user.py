99f09590c70ae63162d49b14c8769f9c73734424
Honza Kopecký <honza.kopecky95@gmail.com>
2018-11-05 14:22:37 +0100

    Store user id and eigen value

--- model/user.py
+++ model/user.py
@@ -1,10 +1,12 @@
 class User:
-    def __init__(self, username):
+    def __init__(self, id, username):
+        self.id = id
         self.username = username
         self.friends = set()
         self.friendsNames = set()
         self.review = None
         self.summary = None
+        self.eigen_value = None
 
     def add_friend(self, user):
         self.friends.add(user)
